56686a0ab2c72a2bbfe24c2fcaa3de7a759e412f
dandyxu <dandyjefferson@gmail.com>
2018-05-08 11:58:36 +0100

    Use form data to create records in create.php

--- basic_php_cms/public/staff/subjects/create.php
+++ basic_php_cms/public/staff/subjects/create.php
@@ -9,10 +9,32 @@ if (is_post_request()) {
 	$position = $_POST['position'] ?? '';
 	$visible = $_POST['visible'] ?? '';
 
-	echo "Form parameter<br />";
-	echo "Menu name: " . $menu_name . "<br />";
-	echo "Position: " . $position . "<br />";
-	echo "Visible: " . $visible . "<br />";
+//	echo "Form parameter<br />";
+//	echo "Menu name: " . $menu_name . "<br />";
+//	echo "Position: " . $position . "<br />";
+//	echo "Visible: " . $visible . "<br />";
+
+	$sql = "INSERT INTO subjects ";
+	$sql .= "(menu_name, position, visible) ";
+	$sql .= "VALUES (";
+	$sql .= "'" . $menu_name . "',";
+	$sql .= "'" . $position . "',";
+	$sql .= "'" . $visible . "'";
+	$sql .= ")";
+
+	$result = mysqli_query($db, $sql);
+	// For INSERT statements, $result is true/false
+
+	if ($result) {
+		$new_id = mysqli_insert_id($db);
+		redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
+
+	}else {
+		// INSERT Failed
+		echo mysqli_error($db);
+		db_disconnect($db);
+		exit;
+	}
 
 } else {
 	redirect_to(url_for('/staff/subjects/new.php'));
