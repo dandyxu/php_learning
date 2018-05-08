c1b17bbb95b3d45ff97d6822b6edd9851d228fb0
dandyxu <dandyjefferson@gmail.com>
2018-05-08 10:36:38 +0100

    Re-structure the show.php

--- basic_php_cms/public/staff/subjects/show.php
+++ basic_php_cms/public/staff/subjects/show.php
@@ -13,10 +13,24 @@ require_once('../../../private/initialize.php');
 $id = $_GET['id'] ?? '1'; // PHP > 7.0
 
 // htmlspecialchars to prevent XSS (Cross-site Scripting)
-echo h($id);
+//echo h($id);
 
 ?>
 
-<a href="show.php?name=<?php echo u('Dandy Xu'); ?>">Link</a><br>
-<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br>
-<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br>
+<?php $page_title = 'Show Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<!--<a href="show.php?name=--><?php //echo u('Dandy Xu'); ?><!--">Link</a><br>-->
+<!--<a href="show.php?company=--><?php //echo u('Widgets&More'); ?><!--">Link</a><br>-->
+<!--<a href="show.php?query=--><?php //echo u('!#*?'); ?><!--">Link</a><br>-->
+
+<div id="content">
+
+    <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+    <div class="subject show">
+
+        Subject ID: <?php echo h($id); ?>
+
+    </div>
+</div>
