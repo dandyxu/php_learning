f1f69c39efd269fbb8abad1ea0fa1697d5e8a924
dandyxu <dandyjefferson@gmail.com>
2018-05-14 16:05:09 +0100

    Display validation errors in edit.php and new.php

--- basic_php_cms/private/initialize.php
+++ basic_php_cms/private/initialize.php
@@ -36,4 +36,5 @@
 	require_once('validation_functions.php');
 
 	$db = db_connect();
+	$errors = [];
 
--- basic_php_cms/public/staff/subjects/edit.php
+++ basic_php_cms/public/staff/subjects/edit.php
@@ -5,6 +5,7 @@ if (!isset($_GET['id'])) {
 }
 
 $id = $_GET['id'];
+//$errors = [];
 
 
 if (is_post_request()) {
@@ -26,17 +27,17 @@ if (is_post_request()) {
 	    redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
     } else {
         $errors = $result;
-        var_dump($errors);
+//        var_dump($errors);
+
     }
 
 } else {
-
 	$subject = find_subject_by_id($id);
-
-	$subject_set = find_all_subjects();
-	$subject_count = mysqli_num_rows($subject_set);
-	mysqli_free_result($subject_set);
 }
+
+$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set);
+mysqli_free_result($subject_set);
 ?>
 
 <?php $page_title = 'Create Subject'; ?>
@@ -48,6 +49,8 @@ if (is_post_request()) {
 
 	<div class="subject edit">
 		<h1>Edit Subject</h1>
+
+        <?php echo display_errors($errors); ?>
 
 		<form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
 			<dl>
